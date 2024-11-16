47e0e7a299f41d0c51cbcc107b17723a171dc40b
sleeper <sleepervoid.c@gmail.com>
2024-11-16 14:33:19 +0000

    update

--- Quiz.py
+++ Quiz.py
@@ -1,3 +1,4 @@
+import csv
 import sqlite3
 import tkinter as tk
 from Jogador import Jogador
@@ -12,6 +13,7 @@ class Quiz():
         self.root = root
         self.root.title('Quiz!')
         self.tela_inicial()
+        self.criar_bd()
 
     def tela_inicial(self):
         title_label = tk.Label(self.root, text="Bem-Vindos ao Quiz!", font=("Arial", 24))
@@ -21,19 +23,23 @@ class Quiz():
 
 
 
-    def criar_banco_dados(self):
+    def criar_bd(self):
         conn = sqlite3.connect('quiz.db')
         c = conn.cursor()
+
+        c.execute('DROP TABLE IF EXISTS perguntas')
+        c.execute('DROP TABLE IF EXISTS jogadores')
+        c.execute('DROP TABLE IF EXISTS resultados')
 
         # Tabela de perguntas
         c.execute('''CREATE TABLE IF NOT EXISTS perguntas (
                         id INTEGER PRIMARY KEY,
-                        question TEXT,
-                        option1 TEXT,
-                        option2 TEXT,
-                        option3 TEXT,
-                        option4 TEXT,
-                        correct INTEGER)''')
+                        pergunta TEXT,
+                        opcao1 TEXT,
+                        opcao2 TEXT,
+                        opcao3 TEXT,
+                        opcao4 TEXT,
+                        correta INTEGER)''')
 
         # Tabela de jogadores
         c.execute('''CREATE TABLE IF NOT EXISTS jogadores (
@@ -52,9 +58,33 @@ class Quiz():
 
         conn.commit()
         conn.close()
-
+        print('BD criada com sucesso')
         # Carregar perguntas do CSV
-        #TODO: carregar perguntas para tabela da BD
+        Quiz.carregar_perguntas(self)
 
+    def carregar_perguntas(self):
+        conn = sqlite3.connect('quiz.db')
+        cursor = conn.cursor()
 
+        try:
+            with open('questions.csv', newline='', encoding='utf-8') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    cursor.execute('''
+                        INSERT INTO perguntas (pergunta, opcao1, opcao2, opcao3, opcao4, correta)
+                        VALUES (?, ?, ?, ?, ?, ?)
+                        ''', (
+                        row['question'],
+                        row['option1'],
+                        row['option2'],
+                        row['option3'],
+                        row['option4'],
+                        int(row['correct'])
+                    ))
+            conn.commit()
+            print('Perguntas carregadas com sucesso')
+        except Exception as e:
+            print(f"Erro: {e}")
+        finally:
+            conn.close()
 
